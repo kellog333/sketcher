64430a119bcf0090d17aa9fb918f8c666060e03c
Kevin Heuser <kevinh@hypersign.com>
2021-10-01 16:49:11 -0500

    Its coming along

--- src/Components/Sketcher.js
+++ src/Components/Sketcher.js
@@ -1,3 +1,58 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
 import paper from 'paper';
 import Tools from './Tools';
+import { Pen } from './Tools/Pen';
 
+class Sketcher extends PureComponent {
+    static PropTypes = {
+        lineColor: PropTypes.string,
+        lineWidth: PropTypes.number,
+        fillColor: PropTypes.string,
+        background: PropTypes.string,
+        opacity: PropTypes.number,
+        undoSteps: PropTypes.number,
+        tool: PropTypes.string,
+        value: PropTypes.object
+    }
+
+    _initTools = () => {
+        this._tools = {};
+        this._tools[Tools.Pen] = new Pen();
+    }
+
+    _doMouseDown = (e) => {
+        this._selectedTool.doMouseDown(e);
+    }
+
+    _doMouseUp = (e) => {
+        this._selectedTool.doMouseUp(e);
+    }
+
+    _doMouseMove = (e) => {
+        this._selectedTool.doMouseMove(e);
+    }
+
+    componentDidMount = () => {
+        let {
+            tool,
+            value,
+            undoSteps
+        } = this.props;
+
+        let canvas = this._canvas;
+
+        this._initTools(canvas);
+
+        let selectedTool = this._tools[tool];
+        this._selectedTool = selectedTool;
+
+        canvas.on('mousedown', this._doMouseDown);
+        canvas.on('mouseup', this._doMouseUp);
+        canvas.on('mousemove', this._doMouseMove);
+        canvas.on('touchstart', this._doMouseDown);
+        canvas.on('touchmove', this._doMouseMove);
+        canvas.on('touchend', this._doMouseUp);
+    }
+
+}
--- src/Components/Tools/Pen.js
+++ src/Components/Tools/Pen.js
@@ -12,33 +12,54 @@ class Pen extends PaperTools {
         this.group = null;
         this.color = props.color;
         this.lineWidth = props.lineWidth;
+        this.tool = props.tool;
     }
     
     doMouseDown(event) {
         let layer = createLayer();
-        local.path = new paper.Path();
-        local.path.strokeColor = this.color;
-        local.path.lineWidth = this.lineWidth;
-        local.path.add(event.point);
+        this.path = new paper.Path();
+        this.path.strokeColor = this.color;
+        this.path.lineWidth = this.lineWidth;
+        this.path.add(event.point);
 
-        local.group.addChild(new paper.Shape.Ellipse({
+        this.group.addChild(new paper.Shape.Ellipse({
             layer: layer,
             center: event.point,
             strokeColor: this.color,
             fillColor: this.color,
             radius: this.lineWidth / 2
         }));
-        layer.addChild(local.group);
+        layer.addChild(this.group);
     }
 
     doMouseMove(event) {
-        if (!local.path) return;
-        local.path.add(event.point);
-        local.path.selected = true;
+        if (!this.path) return;
+        this.path.add(event.point);
+        this.path.selected = true;
     }
 
     doMouseUp(event) {
-        local.path.add(event.point);
-        local.path.simplify();
+        this.path.add(event.point);
+        this.path.simplify();
+        const action = new DrawAction({
+            layer: this.path.layer.name,
+            tool: this.tool,
+            points: this.path.segments.map(seg => {
+                return {
+                    x: seg._point._x,
+                    y: seg._point._y
+                }
+            })
+        });
+
+        history.add(action);
+        this.path.selected = false;
+        this.path = null;
+        this.group = null;
     }
 }
+
+export const Pen = new paper.Tool();
+Pen.doMouseDown = doMouseDown;
+Pen.doMouseMove = doMouseMove;
+Pen.doMouseUp = doMouseUp;
